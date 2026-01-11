c22cd5735fca933dbce0db549c9ce6ff745fee1f
SkidBoi <skid.fanning788@slmails.com>
2026-01-11 18:10:59 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,8 +1,22 @@
-# always clone into default folder
-$env:GIT_DEFAULT = "D:\GitRepos"
+#!/usr/bin/env bash
 
-function gclone {
-    param([string]$url)
-    cd $env:GIT_DEFAULT
-    git clone $url
-}
+INSTALL_DIR="$HOME/.gitloc/bin"
+mkdir -p "$INSTALL_DIR"
+
+cp ./git-loc.py "$INSTALL_DIR/git-loc"
+chmod +x "$INSTALL_DIR/git-loc"
+
+# Create gclone wrapper
+echo -e '#!/usr/bin/env bash\npython3 "$HOME/.gitloc/bin/git-loc" gclone "$@"' > "$INSTALL_DIR/gclone"
+chmod +x "$INSTALL_DIR/gclone"
+
+# Add to PATH if missing
+if ! echo "$PATH" | grep -q "$INSTALL_DIR"; then
+  SHELL_RC="$HOME/.bashrc"
+  if [ -n "$ZSH_VERSION" ]; then
+    SHELL_RC="$HOME/.zshrc"
+  fi
+  echo "export PATH=\$PATH:$INSTALL_DIR" >> "$SHELL_RC"
+fi
+
+echo "Git Location Changer installed with gclone. Restart your terminal."
